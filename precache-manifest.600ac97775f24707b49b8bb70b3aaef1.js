c706d0c29c99a326b27ee3da5f0ebfb1db1827c5
Damien Sim <damien_SIM@tech.gov.sg>
2020-09-23 17:21:06 +0800

    Updates

---
+++ precache-manifest.600ac97775f24707b49b8bb70b3aaef1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72cf45cd7bcd80035014fb957a99153b",
+    "url": "/ui-todo-tutorial/index.html"
+  },
+  {
+    "revision": "8bfc7b0ab9ba3dac00d8",
+    "url": "/ui-todo-tutorial/static/css/main.d2ba111d.chunk.css"
+  },
+  {
+    "revision": "e8fa3bc4ef99f34ff731",
+    "url": "/ui-todo-tutorial/static/js/2.cccd8c0a.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/ui-todo-tutorial/static/js/2.cccd8c0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bfc7b0ab9ba3dac00d8",
+    "url": "/ui-todo-tutorial/static/js/main.007f8a33.chunk.js"
+  },
+  {
+    "revision": "ff9ff1abb2c1f2c1bfcf",
+    "url": "/ui-todo-tutorial/static/js/runtime-main.86cf155b.js"
+  }
+]);
